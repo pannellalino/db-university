02a2275eb02ee23e089997faff1cea45561598b7
pannellalino <lista>
2022-12-20 09:51:38 +0100

    correction

--- db-group-by-query.php
+++ db-group-by-query.php
@@ -11,3 +11,15 @@ GROUP BY `anno`;
 SELECT COUNT(*), `office_address`
 FROM `teachers`
 GROUP BY `office_address`;
+
+3.
+
+SELECT AVG(`vote`), `exam_id`
+FROM `exam_student`
+GROUP BY `exam_id`;
+
+4.
+
+SELECT COUNT(*), `degrees`.`department_id`
+FROM `degrees`
+GROUP BY `degrees`.`department_id`;
